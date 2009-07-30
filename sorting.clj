613b2724e0bcbc83311cca8ebc5d9585ed85e61e
Balint Erdi <balint.erdi@gmail.com>
2009-07-30 11:54:56 +0200

    getting rid of everything that is not needed

--- sorting.clj
+++ sorting.clj
@@ -1,24 +1,6 @@
 (ns sorting
   (:use clojure.contrib.test-is))
 
-(defn swap-if [elts func]
-  (let [a (first elts)
-        b (last elts)]
-    (if (and a b (func a b)) (reverse elts) elts) ; reverse is not lazy!
-  )
-)
-
-; swaps all pairs if func is true for the first of the pair
-(defn swap-all-not-perfect [elts func swapped]
-  (if (< (count elts) 2)
-    (concat swapped elts)
-    (let [[a b] (swap-if (take 2 elts) func)]
-      ;(println (str "A: " a " B: " b " Elts: " elts " Swapped: " swapped))
-      (swap-all-not-perfect (conj (drop 2 elts) b) func (conj swapped a))
-    )
-  )
-)
-
 (defn swap-in-coll [coll index func]
   (cond
     (nil? (nth coll (inc index) nil)) coll
@@ -37,53 +19,8 @@
   ([coll func]
     (swap-all coll 0 func))
   ([coll index func]
-    (do
-      (println "Coll: " coll " Idx: " index)
-      (if (= index (count coll)) coll
-        (let [new_coll (swap-in-coll coll index func)]
-          (do
-            (println "New coll: " new_coll)
-            (recur new_coll (inc index) func))))
-    )
-  )
-)
-
-; the last one in elts is always sorted already so
-; 1. swap-all is executed on elts
-; 2. the last element is consed with sorted
-; 3. bubble-sort is called on the elts minus the last element
-(defn bubble-sort-no-tco
-  "A bubble sort without tail call optimization (TCO)"
-  ([elts]
-    (bubble-sort-no-tco elts []))
-  ([elts sorted]
-    (if (empty? elts)
-      sorted
-      (let [swapped (swap-all elts > [])
-            done (last swapped)
-            others (butlast swapped)]
-        ;(println (str "Done: " done " others: " others " Elts: " elts " Sorted: " sorted))
-        (bubble-sort-no-tco others (cons done sorted))
-      )
-    )
-  )
-)
-
-(defn bubble-sort-tco
-  "A bubble sort with tail call optimization (TCO)"
-  ([elts]
-    (bubble-sort-tco elts []))
-  ([elts sorted]
-    (if (empty? elts)
-      sorted
-      (let [swapped (swap-all elts > [])
-            done (last swapped)
-            others (butlast swapped)]
-        ;(println (str "Done: " done " others: " others " Elts: " elts " Sorted: " sorted))
-        (recur others (cons done sorted))
-      )
-    )
-  )
+    (if (= index (count coll)) coll
+      (recur (swap-in-coll coll index func) (inc index) func)))
 )
 
 (defn bubble-sort [elts]
@@ -91,13 +28,6 @@
   (if (empty? elts)
     []
     (nth (iterate (fn [e] (swap-all e >)) elts) (dec (count elts))))
-)
-
-(deftest test-swap-if
-  (is (= [] (swap-if [] >)))
-  (is (= [1] (swap-if [1] >)))
-  (is (= [1 2] (swap-if [1 2] >)))
-  (is (= [1 2] (swap-if [2 1] >)))
 )
 
 (deftest test-swap-all
@@ -135,4 +65,4 @@
 
 ;(time (take 5 (bubble-sort-no-tco (reverse (range 1000))))) ; 23 seconds
 ;(time (take 5 (bubble-sort-tco (reverse (range 1000))))) ; 22 seconds
-;(time (take 5 (bubble-sort (reverse (range 1000)))))
+;(time (take 5 (bubble-sort (reverse (range 1000))))) ; 769 seconds :(
