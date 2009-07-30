625d45ae147a2233f7d9226c61df61078e1e5f0d
Balint Erdi <balint.erdi@gmail.com>
2009-07-30 11:24:41 +0200

    code made nicer: only using lazy sequences and a recur

--- sorting.clj
+++ sorting.clj
@@ -19,19 +19,35 @@
   )
 )
 
-(defn swap-all
-  ([elts func]
-    (swap-all elts func []))
-  ([elts func swapped]
-    (if (< (count elts) 2)
-      (concat swapped elts)
-      (let [[a b] (swap-if (take 2 elts) func)]
-        ;(println (str "A: " a " B: " b " Elts: " elts " Swapped: " swapped))
-        (swap-all (conj (drop 2 elts) b) func (conj swapped a))
+(defn swap-in-coll [coll index func]
+  (cond
+    (nil? (nth coll (inc index) nil)) coll
+    (func (nth coll index) (nth coll (inc index)))
+      (let [lead (take index coll)
+            swapped-ahead (take 1 (drop (inc index) coll))
+            swapped-behind (take 1 (drop index coll))
+            rest (drop (+ 2 index) coll)]
+            (concat lead swapped-ahead swapped-behind rest)
       )
+    :else coll
+  )
+)
+
+(defn swap-all
+  ([coll func]
+    (swap-all coll 0 func))
+  ([coll index func]
+    (do
+      (println "Coll: " coll " Idx: " index)
+      (if (= index (count coll)) coll
+        (let [new_coll (swap-in-coll coll index func)]
+          (do
+            (println "New coll: " new_coll)
+            (recur new_coll (inc index) func))))
     )
   )
 )
+
 ; the last one in elts is always sorted already so
 ; 1. swap-all is executed on elts
 ; 2. the last element is consed with sorted
@@ -85,13 +101,26 @@
 )
 
 (deftest test-swap-all
-  (is (= [] (swap-all [] > [])))
-  (is (= [1] (swap-all [1] > [])))
-  (is (= [1 2] (swap-all [1 2] > [])))
-  (is (= [1 2] (swap-all [2 1] > [])))
-  (is (= [2 1 3] (swap-all [3 2 1] > [])))
-  (is (= [1 2 3] (swap-all [2 1 3] > [])))
-  (is (= [1 2 3 4] (swap-all [2 1 4 3] > [])))
+  (is (= [] (swap-all [] >)))
+  (is (= [1] (swap-all [1] >)))
+  (is (= [1 2] (swap-all [1 2] >)))
+  (is (= [1 2] (swap-all [2 1] >)))
+  (is (= [2 1 3] (swap-all [3 2 1] >)))
+  (is (= [1 2 3] (swap-all [2 1 3] >)))
+  (is (= [1 2 3 4] (swap-all [2 1 4 3] >)))
+)
+
+(deftest test-swap-in-coll
+  (is (= [] (swap-in-coll [] 0 >)))
+  (is (= [] (swap-in-coll [] 2 >)))
+  (is (= [1] (swap-in-coll [1] 0 >)))
+  (is (= [1] (swap-in-coll [1] 2 >)))
+  (is (= [1 2] (swap-in-coll [1 2] 0 >)))
+  (is (= [1 2] (swap-in-coll [2 1] 0 >)))
+  (is (= [2 1] (swap-in-coll [2 1] 1 >)))
+  (is (= [4 2 3 1] (swap-in-coll [4 3 2 1] 1 >)))
+  (is (= [4 3 1 2] (swap-in-coll [4 3 2 1] 2 >)))
+  (is (= [3 4 2 1] (swap-in-coll [4 3 2 1] 0 >)))
 )
 
 (deftest test-bubble-sort
